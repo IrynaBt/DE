52c3bf25919efb1e4eade6d2889d07dc540397a0
IrynaBt <irina.bt@gmail.com>
2017-03-03 18:51:12 +0200

    task 5

---
+++ task05/README.md
@@ -0,0 +1,21 @@
+Try to write a function that analysis the following text and replaces as many E-mail-addresses as possible with some marker.
+You don't have to store the addresses, just replace them.
+ 
+Sehr geehrter Herr Müller,
+ 
+hier finden Sie meine private E-Mail-Adresse: heinz.mueller@gmx.de. Da ich aber gemerkt hatte, dass dieses System sämtliche E-Mails automatisch entfernt, versuche ich die Adresse anders zu übermitteln.
+Sie erreichen mich also unter heinz.mueller[at]gmx.de oder h e i n z at m u e l l e r . d e. Order aber heinzpunktmueller(at)gmxpunktde.
+ 
+Viele Grüße,
+ 
+Heinz Müller
+ 
+ 
+Dear Mr Müller,
+ 
+here you find my private email: heinz@mueller.gmx.de. As I found out that this system removes the E-mails automatically, therfore I am trying to send you this message in a different way.
+As mentioned you can contact me under this E-mail: heinz.mueller[äT]gmx.de or he i nz {At} mu e ll er . d e. or heinzdotmueller( ad )gmxdot.de.
+ 
+Best regards,
+ 
+Heinz Müller
---
+++ task05/index.php
@@ -0,0 +1,28 @@
+<?php
+$text = 'Sehr geehrter Herr Müller,
+ 
+hier finden Sie meine private E-Mail-Adresse: heinz.mueller@gmx.de. Da ich aber gemerkt hatte, dass dieses System sämtliche E-Mails automatisch entfernt, versuche ich die Adresse anders zu übermitteln.
+Sie erreichen mich also unter heinz.mueller[at]gmx.de oder h e i n z at m u e l l e r . d e. Order aber heinzpunktmueller(at)gmxpunktde.
+ 
+Viele Grüße,
+ 
+Heinz Müller
+
+Dear Mr Müller,
+ 
+here you find my private email: heinz@mueller.gmx.de. As I found out that this system removes the E-mails automatically, therfore I am trying to send you this message in a different way.
+As mentioned you can contact me under this E-mail: heinz.mueller[äT]gmx.de or he i nz {At} mu e ll er . d e. or heinzdotmueller( ad )gmxdot.de.
+ 
+Best regards,
+ 
+Heinz Müller';
+
+$emailHidden = preg_replace(
+    '/[[\sA-Z0-9._%+-]+[ ]?[\(|\[\{]?(@|at|äT|At|ad|\(\sad\s\)|\{)[\)\]\}]?[ ]?[A-Z0-9.-]+[ ]?[\(]?(\.|dot)[)]?[ ]?[A-Z]{0,4}/i',
+    '***E-MAIL HIDDEN***',
+    $text
+);
+
+
+echo $emailHidden;
+?>
